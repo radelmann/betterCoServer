41a7c5f3e6d6c1d36f3dd736c2986a88c8b289dc
radelmann <robadelmann@gmail.com>
2016-04-28 13:16:29 -0700

    added delete user route and controller method

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,10 @@ const config = require('../config');
 
 function tokenForUsr(user) {
   const timeStamp = new Date().getTime();
-  return jwt.encode({ sub: user.id, iat: timeStamp }, config.secret);
+  return jwt.encode({
+    sub: user.id,
+    iat: timeStamp
+  }, config.secret);
 }
 
 exports.signup = function(req, res, next) {
@@ -13,8 +16,8 @@ exports.signup = function(req, res, next) {
 
   if (!email || !password) {
     return res.status(422).send({
-        error: 'email and/or password required'
-      });
+      error: 'email and/or password required'
+    });
   }
 
   User.findOne({
@@ -40,11 +43,44 @@ exports.signup = function(req, res, next) {
         return next(err);
       }
 
-      res.json({token: tokenForUsr(saved)});
+      res.json({
+        token: tokenForUsr(saved)
+      });
     });
   });
 }
 
-exports.signin = function(req,res,next) {
-  res.send({token:tokenForUsr(req.user)});
+exports.delete = function(req, res, next) {
+  var token = req.headers['authorization'];
+  var decoded = jwt.decode(token, config.secret);
+  //decode userid as token, attempt to find user
+  User.findOne({
+      _id: decoded.sub
+    })
+    .then(function(foundUser) {
+      if (foundUser) {
+        foundUser.remove(function(err, deleted) {
+          if (err) {
+            next(err);
+          }
+          res.status(200);
+          res.json(deleted);
+        });
+      } else {
+        res.status(404);
+        res.json({
+          message: "Not Found"
+        });
+      }
+    })
+    .catch(function(error) {
+      next(error);
+    });
+}
+
+
+exports.signin = function(req, res, next) {
+  res.send({
+    token: tokenForUsr(req.user)
+  });
 }
--- router.js
+++ router.js
@@ -13,10 +13,17 @@ const requireAuth = passport.authenticate('jwt', {session : false});
 const requireSignin = passport.authenticate('local', {session : false});
 
 module.exports = function(app) {
+  app.get('/', function(req, res, next) {
+    res.status(200).json({status:"OK"});
+  });
+
   //protected comment routes
   app.get('/comment', requireAuth, Comment.get);
   app.post('/comment', cleanInput, requireAuth, Comment.post);
   app.delete('/comment/:id', requireAuth, Comment.delete);
+
+  //delete user route 
+  app.delete('/user', Auth.delete);
 
   //auth routes
   app.post('/signup', cleanInput, Auth.signup);
