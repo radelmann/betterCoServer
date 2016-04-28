60e648c9f89bd7b380f7386896eeebadcea9cd77
radelmann <robadelmann@gmail.com>
2016-04-28 12:43:07 -0700

    sanitize user input for comment routes

--- router.js
+++ router.js
@@ -1,15 +1,22 @@
 const Auth = require('./controllers/auth');
+const Comment = require('./controllers/comment');
 const passportService = require('./services/passport');
 const passport = require('passport');
+var sanitize = require('mongo-sanitize');
+
+var cleanInput = function(req, res, next) {
+  req.body = sanitize(req.body);
+  next();
+}
 
 const requireAuth = passport.authenticate('jwt', {session : false});
 const requireSignin = passport.authenticate('local', {session : false});
 
 module.exports = function(app) {
   //protected comment routes
-  app.get('/comment', requireAuth,, comment.get);
-  app.post('/comment', requireAuth,, cleanInput, comment.post);
-  app.delete('/comment/:id', requireAuth, comment.delete);
+  app.get('/comment', requireAuth, Comment.get);
+  app.post('/comment', requireAuth, cleanInput, Comment.post);
+  app.delete('/comment/:id', requireAuth, Comment.delete);
 
   //auth routes
   app.post('/signup', Auth.signup);
