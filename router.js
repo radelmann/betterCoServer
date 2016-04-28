5574fefd6aa88bf809b11aa9155d2d494846bfaa
radelmann <robadelmann@gmail.com>
2016-04-28 12:35:06 -0700

    protected comment routes

--- router.js
+++ router.js
@@ -6,11 +6,12 @@ const requireAuth = passport.authenticate('jwt', {session : false});
 const requireSignin = passport.authenticate('local', {session : false});
 
 module.exports = function(app) {
-  //protected routes
-  app.get('/', requireAuth, function(req, res) {
-    res.send({ message: "this is a protected route." });
-  });
-  
+  //protected comment routes
+  app.get('/comment', requireAuth,, comment.get);
+  app.post('/comment', requireAuth,, cleanInput, comment.post);
+  app.delete('/comment/:id', requireAuth, comment.delete);
+
+  //auth routes
   app.post('/signup', Auth.signup);
   app.post('/signin', requireSignin, Auth.signin);
 }
