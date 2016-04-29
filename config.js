d2934722d77118232c7106e268366d8c32692820
radelmann <robadelmann@gmail.com>
2016-04-28 23:07:22 -0700

    update env vars

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   'secret': process.env.SESSION_SECRET || 'dev-secret',
-  'port': process.env.BETTER_CO_PROD_PORT || 3000,
-  'db_url': process.env.BETTER_CO_PROD_DB_URL || 'mongodb://localhost:bettercoServer/bettercoServer'
+  'port': process.env.BETTER_CO_SERVER_PROD_PORT || 3000,
+  'db_url': process.env.BETTER_CO_SERVER_PROD_DB_URL || 'mongodb://localhost:bettercoServer/bettercoServer'
 }
